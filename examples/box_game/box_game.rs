515bd675a357a775aedeefdefe2ebf395d90e037
gschup <georg.schuppe@gmail.com>
2021-08-19 11:07:06 +0200

    refactor: separated game code and app setup code

---
+++ examples/box_game/box_game.rs
@@ -0,0 +1,179 @@
+use bevy::prelude::*;
+use bevy_ggrs::{Rollback, RollbackIdProvider};
+use ggrs::{GameInput, PlayerHandle, SyncTestSession};
+
+const BLUE: Color = Color::rgb(0.8, 0.6, 0.2);
+const ORANGE: Color = Color::rgb(0., 0.35, 0.8);
+const MAGENTA: Color = Color::rgb(0.9, 0.2, 0.2);
+const GREEN: Color = Color::rgb(0.35, 0.7, 0.35);
+const PLAYER_COLORS: [Color; 4] = [BLUE, ORANGE, MAGENTA, GREEN];
+
+const INPUT_UP: u8 = 1 << 0;
+const INPUT_DOWN: u8 = 1 << 1;
+const INPUT_LEFT: u8 = 1 << 2;
+const INPUT_RIGHT: u8 = 1 << 3;
+
+const MOVEMENT_SPEED: f32 = 0.005;
+const MAX_SPEED: f32 = 0.1;
+const FRICTION: f32 = 0.9;
+const PLANE_SIZE: f32 = 5.0;
+const CUBE_SIZE: f32 = 0.2;
+
+pub struct Player {
+    pub handle: u32,
+}
+
+// Components that should be saved/loaded need to implement the `Reflect` trait
+#[derive(Default, Reflect)]
+#[reflect(Component)]
+pub struct Velocity {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+// You can also register resources
+#[derive(Default, Reflect)]
+#[reflect(Component)]
+pub struct FrameCount {
+    pub frame: u32,
+}
+
+// all players get the same input for the synctest, handle is ignored
+#[allow(dead_code)]
+pub fn input(_handle: In<PlayerHandle>, keyboard_input: Res<Input<KeyCode>>) -> Vec<u8> {
+    let mut input: u8 = 0;
+
+    if keyboard_input.pressed(KeyCode::W) {
+        input |= INPUT_UP;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        input |= INPUT_LEFT;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        input |= INPUT_DOWN;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        input |= INPUT_RIGHT;
+    }
+
+    // if your inpute takes more than 8 bit to encode, find some way to encode to bytes
+    // (for example by serializing)
+    vec![input]
+}
+
+/// set up a simple 3D scene
+#[allow(dead_code)]
+pub fn setup_system(
+    mut commands: Commands,
+    mut rip: ResMut<RollbackIdProvider>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    session: Res<SyncTestSession>,
+) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: PLANE_SIZE })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+
+    // player cube - just spawn whatever entity you want, then add a `Rollback` component with a unique id (for example through the `RollbackIdProvider` resource).
+    // Every entity that you want to be saved/loaded needs a `Rollback` component with a unique rollback id.
+    // When loading entities from the past, this extra id is necessary to connect entities over different game states
+    let r = PLANE_SIZE / 4.;
+
+    for handle in 0..session.num_players() {
+        let rot = handle as f32 / session.num_players() as f32 * 2. * std::f32::consts::PI;
+        let x = r * rot.cos();
+        let z = r * rot.sin();
+
+        let mut transform = Transform::default();
+        transform.translation.x = x;
+        transform.translation.y = CUBE_SIZE / 2.;
+        transform.translation.z = z;
+
+        commands
+            .spawn_bundle(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Cube { size: CUBE_SIZE })),
+                material: materials.add(PLAYER_COLORS[handle as usize].into()),
+                transform,
+                ..Default::default()
+            })
+            .insert(Player { handle })
+            .insert(Velocity::default())
+            .insert(Rollback::new(rip.next_id()));
+    }
+
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+// Increases the frame count by 1 every update step. If loading and saving resources works correctly,
+// you should see this resource rolling back, counting back up and finally increasing by 1 every update step
+#[allow(dead_code)]
+pub fn increase_frame_system(mut frame_count: ResMut<FrameCount>) {
+    frame_count.frame += 1;
+}
+
+// System that moves the cubes, added as a rollback system above.
+// Filtering for the rollback component is a good way to make sure your game logic systems
+// only mutate components that are being saved/loaded.
+#[allow(dead_code)]
+pub fn move_cube_system(
+    mut query: Query<(&mut Transform, &mut Velocity, &Player), With<Rollback>>,
+    inputs: Res<Vec<GameInput>>,
+) {
+    for (mut t, mut v, p) in query.iter_mut() {
+        let input = inputs[p.handle as usize].buffer[0];
+        // set velocity through key presses
+        if input & INPUT_UP != 0 && input & INPUT_DOWN == 0 {
+            v.z -= MOVEMENT_SPEED;
+        }
+        if input & INPUT_UP == 0 && input & INPUT_DOWN != 0 {
+            v.z += MOVEMENT_SPEED;
+        }
+        if input & INPUT_LEFT != 0 && input & INPUT_RIGHT == 0 {
+            v.x -= MOVEMENT_SPEED;
+        }
+        if input & INPUT_LEFT == 0 && input & INPUT_RIGHT != 0 {
+            v.x += MOVEMENT_SPEED;
+        }
+
+        // slow down
+        if input & INPUT_UP == 0 && input & INPUT_DOWN == 0 {
+            v.z *= FRICTION;
+        }
+        if input & INPUT_LEFT == 0 && input & INPUT_RIGHT == 0 {
+            v.x *= FRICTION;
+        }
+        v.y *= FRICTION;
+
+        // constrain velocity
+        v.x = v.x.min(MAX_SPEED);
+        v.x = v.x.max(-MAX_SPEED);
+        v.y = v.y.min(MAX_SPEED);
+        v.y = v.y.max(-MAX_SPEED);
+        v.z = v.z.min(MAX_SPEED);
+        v.z = v.z.max(-MAX_SPEED);
+
+        // apply velocity
+        t.translation.x += v.x;
+        t.translation.y += v.y;
+        t.translation.z += v.z;
+
+        // constrain cube to plane
+        t.translation.x = t.translation.x.max(-1. * (PLANE_SIZE - CUBE_SIZE) * 0.5);
+        t.translation.x = t.translation.x.min((PLANE_SIZE - CUBE_SIZE) * 0.5);
+        t.translation.z = t.translation.z.max(-1. * (PLANE_SIZE - CUBE_SIZE) * 0.5);
+        t.translation.z = t.translation.z.min((PLANE_SIZE - CUBE_SIZE) * 0.5);
+    }
+}
--- examples/box_game/box_game_p2p.rs
+++ examples/box_game/box_game_p2p.rs
@@ -1,24 +1,15 @@
 use std::net::SocketAddr;
 
 use bevy::{core::FixedTimestep, prelude::*};
-use bevy_ggrs::{GGRSApp, GGRSPlugin, Rollback, RollbackIdProvider};
-use ggrs::{GameInput, P2PSession, PlayerHandle, PlayerType};
+use bevy_ggrs::{GGRSApp, GGRSPlugin};
+use ggrs::PlayerType;
 use structopt::StructOpt;
 
+mod box_game;
+use box_game::*;
+
 const INPUT_SIZE: usize = std::mem::size_of::<u8>();
-const PLAYER_COLORS: [Color; 2] = [Color::rgb(0.8, 0.6, 0.2), Color::rgb(0., 0.35, 0.8)];
 const FPS: u32 = 60;
-
-const INPUT_UP: u8 = 1 << 0;
-const INPUT_DOWN: u8 = 1 << 1;
-const INPUT_LEFT: u8 = 1 << 2;
-const INPUT_RIGHT: u8 = 1 << 3;
-
-const MOVEMENT_SPEED: f32 = 0.005;
-const MAX_SPEED: f32 = 0.1;
-const FRICTION: f32 = 0.9;
-const PLANE_SIZE: f32 = 5.0;
-const CUBE_SIZE: f32 = 0.2;
 
 // structopt will read command line parameters for u
 #[derive(StructOpt)]
@@ -29,19 +20,6 @@ struct Opt {
     players: Vec<String>,
     #[structopt(short, long)]
     spectators: Vec<SocketAddr>,
-}
-
-struct Player {
-    handle: u32,
-}
-
-// Components that should be saved/loaded need to implement the `Reflect` trait
-#[derive(Default, Reflect)]
-#[reflect(Component)]
-struct Velocity {
-    x: f32,
-    y: f32,
-    z: f32,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -88,7 +66,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(GGRSPlugin)
-        .add_startup_system(setup.system())
+        .add_startup_system(setup_system)
         // add your GGRS session
         .with_p2p_session(p2p_sess)
         // define frequency of game logic update
@@ -98,138 +76,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // register components that will be loaded/saved
         .register_rollback_type::<Transform>()
         .register_rollback_type::<Velocity>()
+        // you can also register resources
+        .register_rollback_type::<FrameCount>()
         // these systems will be executed as part of the advance frame update
-        .add_rollback_system(move_cube.system())
+        .add_rollback_system(move_cube_system)
+        .add_rollback_system(increase_frame_system)
         .run();
 
     Ok(())
-}
-
-/// set up a simple 3D scene
-fn setup(
-    mut commands: Commands,
-    mut rip: ResMut<RollbackIdProvider>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    session: Res<P2PSession>,
-) {
-    // plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: PLANE_SIZE })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..Default::default()
-    });
-
-    // player cube - just spawn whatever entity you want, then add a `Rollback` component with a unique id (for example through the `RollbackIdProvider` resource).
-    // Every entity that you want to be saved/loaded needs a `Rollback` component with a unique rollback id.
-    // When loading entities from the past, this extra id is necessary to connect entities over different game states
-    let r = PLANE_SIZE / 4.;
-
-    for handle in 0..session.num_players() {
-        let rot = handle as f32 / session.num_players() as f32 * 2. * std::f32::consts::PI;
-        let x = r * rot.cos();
-        let z = r * rot.sin();
-
-        let mut transform = Transform::default();
-        transform.translation.x = x;
-        transform.translation.y = CUBE_SIZE / 2.;
-        transform.translation.z = z;
-
-        commands
-            .spawn_bundle(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Cube { size: CUBE_SIZE })),
-                material: materials.add(PLAYER_COLORS[handle as usize].into()),
-                transform,
-                ..Default::default()
-            })
-            .insert(Player { handle })
-            .insert(Velocity::default())
-            .insert(Rollback::new(rip.next_id()));
-    }
-
-    // light
-    commands.spawn_bundle(PointLightBundle {
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..Default::default()
-    });
-    // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
-}
-
-// System that moves the cubes, added as a rollback system above.
-// Filtering for the rollback component is a good way to make sure your game logic systems
-// only mutate components that are being saved/loaded.
-fn move_cube(
-    mut query: Query<(&mut Transform, &mut Velocity, &Player), With<Rollback>>,
-    inputs: Res<Vec<GameInput>>,
-) {
-    for (mut t, mut v, p) in query.iter_mut() {
-        let input = inputs[p.handle as usize].buffer[0];
-        // set velocity through key presses
-        if input & INPUT_UP != 0 && input & INPUT_DOWN == 0 {
-            v.z -= MOVEMENT_SPEED;
-        }
-        if input & INPUT_UP == 0 && input & INPUT_DOWN != 0 {
-            v.z += MOVEMENT_SPEED;
-        }
-        if input & INPUT_LEFT != 0 && input & INPUT_RIGHT == 0 {
-            v.x -= MOVEMENT_SPEED;
-        }
-        if input & INPUT_LEFT == 0 && input & INPUT_RIGHT != 0 {
-            v.x += MOVEMENT_SPEED;
-        }
-
-        // slow down
-        if input & INPUT_UP == 0 && input & INPUT_DOWN == 0 {
-            v.x *= FRICTION;
-        }
-        if input & INPUT_LEFT == 0 && input & INPUT_RIGHT == 0 {
-            v.z *= FRICTION;
-        }
-        v.y *= FRICTION;
-
-        // constrain velocity
-        v.x = v.x.min(MAX_SPEED);
-        v.x = v.x.max(-MAX_SPEED);
-        v.y = v.y.min(MAX_SPEED);
-        v.y = v.y.max(-MAX_SPEED);
-        v.z = v.z.min(MAX_SPEED);
-        v.z = v.z.max(-MAX_SPEED);
-
-        // apply velocity
-        t.translation.x += v.x;
-        t.translation.y += v.y;
-        t.translation.z += v.z;
-
-        // constrain cube to plane
-        t.translation.x = t.translation.x.max(-1. * (PLANE_SIZE - CUBE_SIZE) * 0.5);
-        t.translation.x = t.translation.x.min((PLANE_SIZE - CUBE_SIZE) * 0.5);
-        t.translation.z = t.translation.z.max(-1. * (PLANE_SIZE - CUBE_SIZE) * 0.5);
-        t.translation.z = t.translation.z.min((PLANE_SIZE - CUBE_SIZE) * 0.5);
-    }
-}
-
-// all players get the same input for the synctest, handle is ignored
-fn input(_handle: In<PlayerHandle>, keyboard_input: Res<Input<KeyCode>>) -> Vec<u8> {
-    let mut input: u8 = 0;
-
-    if keyboard_input.pressed(KeyCode::W) {
-        input |= INPUT_UP;
-    }
-    if keyboard_input.pressed(KeyCode::A) {
-        input |= INPUT_LEFT;
-    }
-    if keyboard_input.pressed(KeyCode::S) {
-        input |= INPUT_DOWN;
-    }
-    if keyboard_input.pressed(KeyCode::D) {
-        input |= INPUT_RIGHT;
-    }
-
-    // if your inpute takes more than 8 bit to encode, find some way to encode to bytes
-    // (for example by serializing)
-    vec![input]
 }

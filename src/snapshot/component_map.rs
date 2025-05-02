c5d48af45a977de47712b5838dcf6024544f0cfe
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2025-05-03 01:34:48 +0200

    test: Add better tests for component entity mapping

--- src/snapshot/component_map.rs
+++ src/snapshot/component_map.rs
@@ -97,18 +97,20 @@ mod tests {
         tests::{advance_frame, load_world, save_world},
         AddRollbackCommandExtension, AdvanceWorld, RollbackApp, SnapshotPlugin,
     };
+    use bevy::log::LogPlugin;
 
     #[derive(Resource, Default)]
     enum Input {
         #[default]
         None,
         SpawnFriend,
+        DespawnFriends,
     }
 
     #[derive(Component, MapEntities, Clone, Copy)]
-    struct Likes(Entity);
+    struct Likes(#[entities] Entity);
 
-    #[derive(Component)]
+    #[derive(Component, Clone, Copy)]
     struct Friend;
 
     #[derive(Component)]
@@ -122,12 +124,27 @@ mod tests {
         // check if there is one and only one friend
         if let Ok(friend) = friends.single() {
             commands.entity(player.entity()).insert(Likes(friend));
+        } else {
+            // if there are no friends, remove the Likes component from the player
+            commands.entity(player.entity()).remove::<Likes>();
         }
     }
 
     fn spawn_friend(mut commands: Commands, inputs: Res<Input>) {
         if let Input::SpawnFriend = *inputs {
             commands.spawn(Friend).add_rollback();
+        }
+    }
+
+    fn despawn_friends(
+        inputs: Res<Input>,
+        friends: Query<Entity, With<Friend>>,
+        mut commands: Commands,
+    ) {
+        if let Input::DespawnFriends = *inputs {
+            for friend in &friends {
+                commands.entity(friend).despawn();
+            }
         }
     }
 
@@ -183,6 +200,65 @@ mod tests {
         // advance to frame 1 again, spawns a friend (a new entity, though)
         app.world_mut().insert_resource(Input::SpawnFriend);
         advance_frame(app.world_mut());
+
+        {
+            let friend_entity = get_friend_entity(app.world_mut()).unwrap();
+            let liked_entity = get_liked_entity(app.world_mut()).unwrap();
+            assert_eq!(friend_entity, liked_entity);
+            assert_ne!(friend_entity, initial_friend_entity);
+            assert_ne!(liked_entity, initial_liked_entity);
+        }
+    }
+
+    #[test]
+    fn restores_entity_mappings_after_despawning() {
+        let mut app = App::new();
+        app.add_plugins(MinimalPlugins);
+        app.add_plugins(LogPlugin::default());
+        app.add_plugins(SnapshotPlugin);
+        app.rollback_component_with_copy::<Friend>();
+        app.rollback_component_with_copy::<Likes>();
+        app.update_component_with_map_entities::<Likes>();
+        app.add_systems(
+            AdvanceWorld,
+            (spawn_friend, like_single_friend, despawn_friends).chain(),
+        );
+        app.add_systems(Startup, spawn_player);
+        app.update();
+
+        let get_friend_entity = |world: &mut World| {
+            world
+                .query_filtered::<Entity, With<Friend>>()
+                .single(world)
+                .ok()
+        };
+
+        let get_liked_entity = |world: &mut World| {
+            world
+                .query::<&Likes>()
+                .single(world)
+                .ok()
+                .map(|likes| likes.0)
+        };
+
+        // advance to frame 1, spawns a friend
+        app.world_mut().insert_resource(Input::SpawnFriend);
+        advance_frame(app.world_mut());
+
+        let initial_friend_entity = get_friend_entity(app.world_mut()).unwrap();
+        let initial_liked_entity = get_liked_entity(app.world_mut()).unwrap();
+        assert_eq!(initial_friend_entity, initial_liked_entity);
+
+        save_world(app.world_mut()); // save frame 1
+
+        // advance to frame 2, despawns a friend
+        app.world_mut().insert_resource(Input::DespawnFriends);
+        advance_frame(app.world_mut());
+
+        assert_eq!(get_friend_entity(app.world_mut()), None);
+
+        // roll back to frame 1, should restore the friend entity
+        load_world(app.world_mut(), 1);
 
         {
             let friend_entity = get_friend_entity(app.world_mut()).unwrap();

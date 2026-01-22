ffba1651e59c4e0e13ea5629f6ac5ed94d087e26
Adam Mitchell <adammitchelldev@gmail.com>
2026-01-22 16:29:33 +0000

    Make component and commands available (not sure that the component needs to be public?)

--- src/snapshot/mod.rs
+++ src/snapshot/mod.rs
@@ -50,6 +50,7 @@ pub use strategy::*;
 
 pub mod prelude {
     pub use super::{Checksum, LoadWorldSystems, SaveWorldSystems};
+    pub use super::despawn::{RollbackDespawned, RollbackDespawnCommandExtension};
 }
 
 /// Label for the schedule which loads and overwrites a snapshot of the world.
